577ee3372524e925e0b0b40397419494a675d59a
omairapalacios <cpalaciosanchez@hotmail.com>
2021-07-03 01:15:52 -0500

    add principal views

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,34 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Loading from '../components/Loading.vue';
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+  scrollBehavior: function(to) {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
+  routes: [
+    {
+      path: '/',
+      name: 'loading',
+      component: Loading,
+    },
+    {
+      path: '/main',
+      name: 'root',
+      component: () => import('@/views/Root'),
+    },
+    {
+      path: '/archive',
+      name: 'archive',
+      component: () => import('@/views/Archive'),
+    },
+  ],
+});
 
-export default router
+export default router;
