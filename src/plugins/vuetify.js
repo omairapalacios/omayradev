92896c152106bd503e4fe2530155afece5397b25
omairapalacios <cpalaciosanchez@hotmail.com>
2021-07-03 03:02:22 -0500

    update vuetify themes :art:

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        accent: '#48E9FF',
+        amber: '#FFCC19'
+      },
+      dark: {
+        accent: '#48E9FF',
+        amber: '#FFCC19'
+      },
+    },
+  },
 });
